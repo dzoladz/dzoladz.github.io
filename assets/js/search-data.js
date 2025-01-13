0581106750deab1193e8b6a50c86500bf9c99611
dzoladz <derek@derekzoladz.com>
2025-01-13 02:38:30 +0000

    Deploying to gh-pages from @ dzoladz/dzoladz.github.io@3b9f29d020f339c4d1c1bec8d7afe50afe143d43 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
+        },{id: "nav-projects",
+          title: "Projects",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "post-blank-topographies",
       
         title: "Blank Topographies",
@@ -47,17 +54,6 @@ ninja.data = [{
       handler: () => {
         
           window.location.href = "/blog/2023/apps/";
-        
-      },
-    },{id: "post-apps-i-39-m-taking-into-2023",
-      
-        title: "Apps I&#39;m Taking into 2023",
-      
-      description: "A short list of applications that I&#39;ve found useful in 2022.",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2022/apps/";
         
       },
     },{id: "post-hello-oplin",
@@ -236,49 +232,14 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-lingo",
+          title: 'Lingo',
+          description: "Mobile Application Developed for ILEADUSA-OHIO 2013",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/projects/lingo/";
+            },},{id: "projects-augmented-reality-at-columbus-state-library-photo-essay",
+          title: 'Augmented Reality at Columbus State Library [Photo Essay]',
+          description: "Augmented Reality Exhibit at Columbus State Community College Library",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/thurgood/";
             },},];
